9c6485d62b0c05cc70c7826e151a7737e3c0ca92
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2023-01-29 20:52:23 +0700

    feat: refactor home screen

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../widgets/hotel_carousel.dart';
-import '../widgets/destination_carousel.dart';
+import '../widgets/destinations/destination_carousel.dart';
+import '../utils/home.info.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,74 +10,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
   int _currentTab = 0;
   final int _currentHour = DateTime.now().hour;
-
-  final List<IconData> _icons = [
-    FontAwesomeIcons.plane,
-    FontAwesomeIcons.bed,
-    FontAwesomeIcons.walking,
-    FontAwesomeIcons.biking,
-  ];
-
-  Widget _buildIcon(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        height: 60.0,
-        width: 60.0,
-        decoration: BoxDecoration(
-          color: _selectedIndex == index
-              ? const Color(0xFFD8ECF1)
-              : const Color.fromARGB(255, 243, 243, 240),
-          // color: const Theme.of(context).accentColor
-          borderRadius: BorderRadius.circular(30.0),
-          border: Border.all(
-            width: 0.5,
-            color: Colors.blueGrey.shade100,
-          ),
-        ),
-        child: Icon(
-          _icons[index],
-          size: 25.0,
-          color: _selectedIndex == index
-              ? Theme.of(context).primaryColor
-              : const Color(0xFFB4C1C4),
-        ),
-      ),
-    );
-  }
+  final TextStyle _sloganStyle =
+      const TextStyle(fontSize: 30.0, fontFamily: 'DancingScript');
 
   Widget _buildAppbarGreetingByHour(int hour) {
-    String greeting = "Welcome, Tuan Vu";
-    IconData icons;
-    if (hour <= 12 && hour >= 5) {
-      greeting = "Good morning, Tuan Vu";
-      icons = Icons.wb_sunny;
-    } else if ((hour > 12) && (hour <= 17)) {
-      greeting = "Good afternoon, Tuan Vu";
-      icons = Icons.wb_sunny;
-    } else if ((hour > 17) && (hour < 23)) {
-      greeting = "Good evening, Tuan Vu";
-      icons = Icons.nights_stay;
-    } else {
-      greeting = "Good night, Tuan Vu";
-      icons = Icons.nights_stay;
-    }
+    Greeting greeting = HomeInfoUtil().greetingByHour(hour);
     return Row(
       children: [
         Text(
-          greeting,
-          style: const TextStyle(fontSize: 18.5),
+          greeting.text,
+          style: const TextStyle(fontSize: 18.5, fontFamily: 'Mukta'),
         ),
         const SizedBox(width: 6.0),
         Icon(
-          icons,
+          greeting.icons,
           size: 28.5,
           color: Colors.yellow.shade500,
         ),
@@ -87,111 +36,70 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: _buildAppbarGreetingByHour(_currentHour),
-          backgroundColor: Theme.of(context).primaryColorLight,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.notifications, size: 28.5),
-              tooltip: 'Notification',
-              onPressed: () {
-                // ScaffoldMessenger.of(context).showSnackBar(
-                //     const SnackBar(content: Text('This is a snackbar')));
-              },
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            children: <Widget>[
-              Padding(
-                // padding: EdgeInsets.only(left: 20.0, right: 120.0), iphone 11 pro max,
-                padding: const EdgeInsets.only(
-                  left: 40.0,
-                  right: 40.0,
-                ), // xiaomi
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Let's explore the world",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontFamily: 'DancingScript',
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 170.0),
-                      child: Row(
-                        children: const [
-                          Text(
-                            "with us !",
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              fontFamily: 'DancingScript',
-                            ),
-                          ),
-                          SizedBox(width: 7.5),
-                          Icon(
-                            Icons.travel_explore_sharp,
-                            size: 30.0,
-                            color: Colors.lightGreen,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //   children: _icons
-              //       .asMap()
-              //       .entries
-              //       .map(
-              //         (MapEntry map) => _buildIcon(map.key),
-              //       )
-              //       .toList(),
-              // ),
-              const SizedBox(height: 5.0),
-              HotelCarousel(),
-              const SizedBox(height: 15.0),
-              DestinationCarousel(),
-            ],
+      appBar: AppBar(
+        title: _buildAppbarGreetingByHour(_currentHour),
+        backgroundColor: Theme.of(context).primaryColorLight,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications, size: 28.5),
+            tooltip: 'Notification',
+            onPressed: () {
+              // ScaffoldMessenger.of(context).showSnackBar(
+              //     const SnackBar(content: Text('This is a snackbar')));
+            },
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Theme.of(context).primaryColorLight,
-          currentIndex: _currentTab,
-          onTap: (int value) {
-            setState(() {
-              _currentTab = value;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                size: 30.0,
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Let's explore the world", style: _sloganStyle),
+                  Container(
+                    margin: const EdgeInsets.only(left: 170.0),
+                    child: Row(
+                      children: [
+                        Text("with us !", style: _sloganStyle),
+                        const SizedBox(width: 7.5),
+                        const Icon(
+                          Icons.travel_explore_sharp,
+                          size: 30.0,
+                          color: Colors.lightGreen,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              label: "Home",
             ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.history_edu_outlined,
-                size: 30.0,
-              ),
-              label: "Booking",
-            ),
-            BottomNavigationBarItem(
-              icon: CircleAvatar(
-                radius: 15.0,
-                backgroundImage: AssetImage('assets/images/duck.jpg'),
-              ),
-              label: "Personal",
-            ),
+            const SizedBox(height: 5.0),
+            HotelCarousel(),
+            const SizedBox(height: 15.0),
+            DestinationCarousel(),
           ],
-        ));
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColorLight,
+        currentIndex: _currentTab,
+        onTap: (int value) {
+          setState(() {
+            _currentTab = value;
+          });
+        },
+        items: HomeInfoUtil()
+            .btmNavIcons
+            .map((e) => BottomNavigationBarItem(
+                  icon: e.icon,
+                  label: e.label,
+                ))
+            .toList(),
+      ),
+    );
   }
 }
---
+++ lib/utils/home.info.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BtmNavInfo {
+  Widget icon;
+  String label;
+  BtmNavInfo({required this.icon, required this.label});
+}
+
+class Greeting {
+  String text;
+  IconData icons;
+  Greeting({required this.text, required this.icons});
+}
+
+class HomeInfoUtil {
+  // futureL: call api to get user avatar instead of hard-coded
+  final List<BtmNavInfo> btmNavIcons = [
+    BtmNavInfo(icon: const Icon(Icons.home, size: 30.0), label: "Home"),
+    BtmNavInfo(
+      icon: const Icon(Icons.history_edu_outlined, size: 30.0),
+      label: "Booking",
+    ),
+    BtmNavInfo(
+      icon: const CircleAvatar(
+        radius: 15.0,
+        backgroundImage: AssetImage('assets/images/duck.jpg'),
+      ),
+      label: "Personal",
+    ),
+  ];
+
+  Greeting greetingByHour(int hour) {
+    Greeting greeting = Greeting(text: "Welcome, Tuan Vu", icons: Icons.block);
+    if (hour <= 12 && hour >= 5) {
+      greeting.text = "Good morning, Tuan Vu";
+      greeting.icons = Icons.wb_sunny;
+    } else if ((hour > 12) && (hour <= 17)) {
+      greeting.text = "Good afternoon, Tuan Vu";
+      greeting.icons = Icons.wb_sunny;
+    } else if ((hour > 17) && (hour < 23)) {
+      greeting.text = "Good evening, Tuan Vu";
+      greeting.icons = Icons.nights_stay;
+    } else {
+      greeting.text = "Good night, Tuan Vu";
+      greeting.icons = Icons.nights_stay;
+    }
+    return greeting;
+  }
+}
